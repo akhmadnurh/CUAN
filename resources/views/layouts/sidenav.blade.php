07c9041a0ae2133374e321559020c11d4702c9b5
afrisco <afriscoardytia@gmail.com>
2021-08-30 19:19:58 +0700

    menambahkan tambah catatan di dashboard

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -11,7 +11,10 @@
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
     <link rel="stylesheet" href="./css/dash-style.css">
+
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.0/css/jquery.dataTables.min.css"> --}}
 
     <title>Hello, world!</title>
 </head>
@@ -19,7 +22,7 @@
 <body>
     <nav class="navbar navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">CUAN</a>
+            <a class="navbar-brand" href="#"><i>CUAN</i></a>
             <a class="nav-user" href="/">
                 <i class="bi bi-person-circle"></i>
                 Bootstrap
@@ -39,47 +42,30 @@
                         id="menu">
                         <li class="nav-item">
                             <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Hutang
-                                    Pihutang</span>
+                                <i class="fs-5 bi-cash-coin"></i>
+                                <span class="ms-1 d-none d-sm-inline">Hutang Piutang</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi-speedometer2"></i>
+                                <i class="fs-5 bi-gear-fill"></i>
                                 <span class="ms-1 d-none d-sm-inline">Pengaturan</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi-speedometer2"></i>
+                                <i class="fs-5 bi-clock-history"></i>
                                 <span class="ms-1 d-none d-sm-inline">Riwayat</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi-speedometer2"></i>
+                                <i class="fs-5 bi-box-arrow-right"></i>
                                 <span class="ms-1 d-none d-sm-inline">Logout</span>
                             </a>
                         </li>
                     </ul>
                     <hr>
-                    <!-- <div class="dropdown pb-4">
-                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                            id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30"
-                                class="rounded-circle">
-                            <span class="d-none d-sm-inline mx-1">loser</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                            <li><a class="dropdown-item" href="#">New project...</a></li>
-                            <li><a class="dropdown-item" href="#">Settings</a></li>
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                        </ul>
-                    </div> -->
                 </div>
             </div>
             <div class="col py-3">
@@ -91,6 +77,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
     </script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js">
+    </script>
+
+    <script>
+        $(document).ready(function () {
+            $('#example').DataTable({
+                "paging": false,
+                "searching": false,
+                "info": false,
+                "responsive": true
+            });
+        });
+
+    </script>
+
+
+
 </body>
 
 </html>
