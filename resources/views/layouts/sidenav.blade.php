e7798b0b9e4c794f92666efc2f6b2ade27219cc6
afrisco <afriscoardytia@gmail.com>
2021-08-31 17:54:23 +0700

    menambahkan sidebar toogle hide

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -1,99 +1,56 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-
+    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.11.0/css/jquery.dataTables.min.css"> --}}
+    <link rel="stylesheet" href="./css/sidenav-style.css">
     <link rel="stylesheet" href="./css/dash-style.css">
-
-    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.0/css/jquery.dataTables.min.css"> --}}
-
-    <title>Hello, world!</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-dark">
+
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><i>CUAN</i></a>
-            <a class="nav-user" href="/">
-                <i class="bi bi-person-circle"></i>
-                Bootstrap
-            </a>
+            <a class="navbar-brand" href="#">Navbar</a>
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-person-circle"></i>
+                    User</a>
+                <button class="btn btn-toogle" type="button" onclick="openNav()">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
         </div>
     </nav>
 
-    <div class="container-fluid">
-        <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 side">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <a href="/"
-                        class="d-flex align-items-center pb-3 pt-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span class="fs-5 d-none d-sm-inline">Dashboard</span>
-                    </a>
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-                        id="menu">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-5 bi-cash-coin"></i>
-                                <span class="ms-1 d-none d-sm-inline">Hutang Piutang</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-5 bi-gear-fill"></i>
-                                <span class="ms-1 d-none d-sm-inline">Pengaturan</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-5 bi-clock-history"></i>
-                                <span class="ms-1 d-none d-sm-inline">Riwayat</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ url('logout') }}" class="nav-link align-middle px-0">
-                                <i class="fs-5 bi-box-arrow-right"></i>
-                                <span class="ms-1 d-none d-sm-inline">Logout</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <hr>
-                </div>
-            </div>
-            <div class="col py-3">
-                @yield('container')
-            </div>
-        </div>
+    <div id="mySidebar" class="sidebar">
+        <a href="/">Dashboard</a>
+        <a href="#">Hutang Piutang</a>
+        <a href="#">Pengaturan</a>
+        <a href="#">Riwayat</a>
+        <a href="{{ url('logout') }}">Logout</a>
+    </div>
+
+    <div id="main" class="main">
+        @yield('container')
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
     </script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js">
-    </script>
-
+    <script src="./js/sidenav.js"></script>
+    {{-- <script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script>
         $(document).ready(function () {
-            $('#example').DataTable({
-                "paging": false,
-                "searching": false,
-                "info": false,
-                "responsive": true
-            });
+            $('#example').DataTable();
         });
 
-    </script>
-
-
-
+    </script> --}}
 </body>
 
 </html>
